0f51e344e5b087e604c77f942fc47aece65e8154
Randy <llobera92@gmail.com>
2016-02-12 16:48:17 -0500

    Implement Student-Records-Preview design.

--- ClassAttendanceManager/Student-Records-Preview.aspx
+++ ClassAttendanceManager/Student-Records-Preview.aspx
@@ -5,23 +5,63 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 155px;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
-        <div style="text-align: center; background-color: #CCCCCC;" aria-checked="undefined">
+        <div>
+            <div style="text-align: center; background-color: #CCCCCC;" aria-checked="undefined">
+                <br />
+                <asp:Label ID="header" runat="server" Text="Class Attendance Manager" Font-Bold="True" Font-Size="XX-Large"></asp:Label>
+                <hr />
+            </div>
             <br />
-            <asp:Label ID="header" runat="server" Text="Class Attendance Manager" Font-Bold="True" Font-Size="XX-Large"></asp:Label>
-            <hr />
+            <div style="text-align: center;">
+                <asp:Label ID="Label1" runat="server" Font-Bold="False" Font-Size="X-Large" Text="Student Record Preview"></asp:Label>
+                <br />
+                <asp:Label ID="Label2" runat="server" Font-Size="Medium" Font-Underline="True" Text="Please fill up all the informtation bellow:"></asp:Label>
+                <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+                <hr />
+                <br />
+            </div>
+
+            <table>
+                <tr>
+                    <td class="auto-style1">
+                        <asp:Label ID="Label14" runat="server" Text="Date: "></asp:Label>
+                        <br />
+                        <asp:Label ID="Label4" runat="server" Text="Time: "></asp:Label>
+                        <br />
+                        <asp:Label ID="Label6" runat="server" Text="Your name: "></asp:Label>
+                        <br />
+                        <asp:Label ID="Label8" runat="server" Text="Your student id: "></asp:Label>
+                        <br />
+                        <asp:Label ID="Label5" runat="server" Text="Session: "></asp:Label>
+                        <br />
+                        <asp:Label ID="Label12" runat="server" Text="Your porpouse today: "></asp:Label>
+                        <br />
+                    </td>
+                    <td>
+                        <asp:Label ID="lblDate" runat="server" Text=""></asp:Label>
+                        <br />
+                        <asp:Label ID="lblTime" runat="server" Text=""></asp:Label>
+                        <br />
+                        <asp:Label ID="lblName" runat="server" Text=""></asp:Label>
+                        <br />
+                        <asp:Label ID="lblId" runat="server" Text=""></asp:Label>
+                        <br />
+                        <asp:Label ID="lblClockInOut" runat="server" Text=""></asp:Label>
+                        <br />
+                        <asp:Label ID="lblActivities" runat="server" Text=""></asp:Label>
+                        <br />
+                    </td>
+                </tr>
+            </table>
         </div>
-        <br />
-        <div style="text-align:center;">
-            <asp:Label ID="Label1" runat="server" Font-Bold="False" Font-Size="X-Large" Text="Student Record Preview"></asp:Label>
-            <br />
-            <asp:Label ID="Label2" runat="server" Font-Size="Medium" Font-Underline="True" Text="Please fill up all the informtation bellow:"></asp:Label>
-            <hr />                        
-        </div>        
-    </div>
     </form>
 </body>
 </html>
--- ClassAttendanceManager/Student-Records-Preview.aspx.designer.cs
+++ ClassAttendanceManager/Student-Records-Preview.aspx.designer.cs
@@ -47,5 +47,122 @@ namespace ClassAttendanceManager {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Label14 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label14;
+        
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Label6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// Label8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+        
+        /// <summary>
+        /// Label5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// Label12 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label12;
+        
+        /// <summary>
+        /// lblDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDate;
+        
+        /// <summary>
+        /// lblTime control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTime;
+        
+        /// <summary>
+        /// lblName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblName;
+        
+        /// <summary>
+        /// lblId control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblId;
+        
+        /// <summary>
+        /// lblClockInOut control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblClockInOut;
+        
+        /// <summary>
+        /// lblActivities control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblActivities;
     }
 }
